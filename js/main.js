c782f77fda40e0cebfe14945bb533394c728abc1
unhIT7 <unhang00@gmail.com>
2023-11-02 17:00:06 +0900

    content 자동슬라이드 생성

--- js/main.js
+++ js/main.js
@@ -9,6 +9,87 @@ storeClose.addEventListener('click', () => {
   storeModal.classList.remove('modal');
 });
 
+// 위아래 스크롤
+gsap.registerPlugin(ScrollTrigger);
+
+const additionalY = { val: 0 };
+let additionalYAnim;
+let offset = 0;
+const colsL = gsap.utils.toArray(".contents-left");
+const colsR = gsap.utils.toArray(".contents-right");
+
+colsL.forEach((col, i) => {
+  const images = col.childNodes;
+
+  // DUPLICATE IMAGES FOR LOOP
+  images.forEach((image) => {
+    let clone = image.cloneNode(true);
+    col.appendChild(clone);
+  });
+
+  // SET ANIMATION
+  images.forEach((item) => {
+    let columnHeight = item.parentElement.clientHeight;
+    let direction = i % 2 !== 0 ? "+=" : "-="; // Change direction for odd columns
+
+    gsap.to(item, {
+      y: direction + Number(columnHeight / 2),
+      duration: 20,
+      repeat: -1,
+      ease: "none",
+      modifiers: {
+        y: gsap.utils.unitize((y) => {
+          if (direction == "+=") {
+            offset += additionalY.val;
+            y = (parseFloat(y) - offset) % (columnHeight * 0.5);
+          } else {
+            offset += additionalY.val;
+            y = (parseFloat(y) + offset) % -Number(columnHeight * 0.5);
+          }
+
+          return y;
+        })
+      }
+    });
+  });
+});
+colsR.forEach((col, i) => {
+  const images = col.childNodes;
+
+  // DUPLICATE IMAGES FOR LOOP
+  images.forEach((image) => {
+    let clone = image.cloneNode(true);
+    col.appendChild(clone);
+  });
+
+  // SET ANIMATION
+  images.forEach((item) => {
+    let columnHeight = item.parentElement.clientHeight;
+    let direction = i % 2 !== 0 ? "-=" : "+="; // Change direction for odd columns
+
+    gsap.to(item, {
+      y: direction + Number(columnHeight / 2),
+      duration: 20,
+      repeat: -1,
+      ease: "none",
+      modifiers: {
+        y: gsap.utils.unitize((y) => {
+          if (direction == "+=") {
+            offset += additionalY.val;
+            y = (parseFloat(y) - offset) % (columnHeight * 0.5);
+          } else {
+            offset += additionalY.val;
+            y = (parseFloat(y) + offset) % -Number(columnHeight * 0.5);
+          }
+
+          return y;
+        })
+      }
+    });
+  });
+});
+// 위아래스크롤 끝
+
 let tabButtons = document.querySelectorAll('.brand-tab-btn');
 // console.log(tabButtons)
 
